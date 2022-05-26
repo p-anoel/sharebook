fbb049d0a968c2524c200ef1a88361befa2be79a
Pierre-Axel <pierreaxel.noel@gmail.com>
2022-05-26 21:36:20 +0000

    :tada:Initial commit

---
+++ src/main/java/com/udemy/demo/book/Book.java
@@ -0,0 +1,11 @@
+package com.udemy.demo.book;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+    private String title;
+    private Category category;
+}
---
+++ src/main/java/com/udemy/demo/book/Category.java
@@ -0,0 +1,12 @@
+package com.udemy.demo.book;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Category {
+    private String label;
+}

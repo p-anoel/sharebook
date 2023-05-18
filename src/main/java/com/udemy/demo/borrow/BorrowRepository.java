f9cf20b144ebeac6ed7e68af908d1f1d006b1672
panoel <pierreaxel.noel@gmail.com>
2023-05-18 03:56:40 +0200

    #44 - Implementation du BorrowRepository

--- src/main/java/com/udemy/demo/borrow/BorrowRepository.java
+++ src/main/java/com/udemy/demo/borrow/BorrowRepository.java
@@ -1,4 +1,13 @@
 package com.udemy.demo.borrow;
 
-public interface BorrowRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BorrowRepository extends CrudRepository<Borrow, Integer> {
+    List<Borrow> findByBorrowerId(Integer borrowerId);
+    List<Borrow> findByBookId(Integer bookId);
+
 }

f0311d63d0d9f6151def37cfc926da6a7520dfea
panoel <pierreaxel.noel@gmail.com>
2023-05-18 03:49:50 +0200

    #42 - Implementation du BookRepository

--- src/main/java/com/udemy/demo/book/BookRepository.java
+++ src/main/java/com/udemy/demo/book/BookRepository.java
@@ -1,5 +1,12 @@
 package com.udemy.demo.book;
 
-public class BookRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Integer> {
+    List<Book> findByStatusAndUserIdNotAndDeletedFalse(BookStatus status, Integer userId);
+    List<Book> findByUserIdAndDeletedFalse(Integer id);
 }

121cb4e92a4f0cc5581bed11446de3aa9545e3ae
panoel <pierreaxel.noel@gmail.com>
2024-02-16 15:20:28 +0100

    feat: #72 - retour sur l'inscription

--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -1,9 +1,16 @@
 package com.udemy.demo.user;
 
+import com.udemy.demo.jwt.JwtController;
+import com.udemy.demo.jwt.JwtFilter;
+import com.udemy.demo.jwt.JwtUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +23,38 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    JwtController jwtController;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
     @PostMapping(value = "/users")
     public ResponseEntity addUser (@Valid @RequestBody UserInfo userInfo){
 
-        UserInfo user = userRepository.findOneByEmail(userInfo.getEmail());
+        UserInfo existingUser = userRepository.findOneByEmail(userInfo.getEmail());
 
-        if (user != null){
+        if (existingUser != null){
             return new ResponseEntity("User already existing !", HttpStatus.BAD_REQUEST);
         }
 
-        UserInfo userSaved = userRepository.save(user);
+        UserInfo user = saveUser(userInfo);
+        Authentication authentication = jwtController.logUser(userInfo.getEmail(), userInfo.getPassword());
+        String jwt = jwtUtils.generateToken(authentication);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
 
-        return new ResponseEntity(userSaved, HttpStatus.CREATED);
+        return new ResponseEntity(user, HttpStatus.CREATED);
+    }
+
+    private UserInfo saveUser(UserInfo userInfo){
+        UserInfo user = new UserInfo();
+        user.setEmail(userInfo.getEmail());
+        user.setPassword(new BCryptPasswordEncoder().encode(userInfo.getPassword()));
+        user.setLastName(StringUtils.capitalize(userInfo.getLastName()));
+        user.setFistName(StringUtils.capitalize(userInfo.getFistName()));
+        userRepository.save(user);
+        return user;
     }
 
 }
